1342717d789ab74dea2d01338ff94e1be2c9e33e
Priyabrata Khan <93465026+priyabratakhan@users.noreply.github.com>
2023-01-09 23:26:19 +0530

    header

---
+++ ui/src/components/header/Header.jsx
@@ -0,0 +1,36 @@
+import "./header.css";
+import varal from '../../images/Group 114.png';
+import searchb from '../../images/search.png';
+import Ellipse_16 from '../../images/Ellipse 16.png';
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="varal">
+          <img src={varal} alt="" />
+        </div>
+        <div className="home">
+          <p>Home</p>
+          <p>Service</p>
+          <p>Pricing</p>
+          <p>About Us</p>
+          <button>Start a Company</button>
+        </div>
+      </div>
+      {/* searchBox */}
+      <div className="header1">
+        <div className="leftdot">
+            <img src={Ellipse_16} alt=""/>
+        </div>
+        <div className="search">
+            <div className="searchbox">
+                <img src={searchb} />
+                <p>Search a Team | Topic</p>
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { Header };
